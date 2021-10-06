53723401d184e42ff935baf55380aa430b6fe121
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-06 17:01:42 +0200

    added __str__ method to transaction

--- transaction.py
+++ transaction.py
@@ -5,4 +5,7 @@ class Transaction:
         self.sender = sender
         self.receiver = receiver
         self.amount = amount
-        self.timestamp = time.time()
+        self.timestamp = time()
+
+    def __str__(self):
+        return f"{self.sender} paid {self.receiver} {self.amount}$"
