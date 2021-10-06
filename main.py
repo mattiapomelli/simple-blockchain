1ccc61ab62cf79900fa86ed523544985c8a037fd
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-06 15:54:03 +0200

    added __str__ method to block

--- main.py
+++ main.py
@@ -9,14 +9,24 @@ class Block:
         self.previous_hash = previous_hash
         self.nonce = 0
     
-    def compute_hash(self):
-        hash = sha256()
-        hash.update(
+    def hash(self):
+        h = sha256()
+        h.update(
             str(self.data).encode('utf-8') +
             str(self.timestamp).encode('utf-8') +
             str(self.previous_hash).encode('utf-8') +
             str(self.nonce).encode('utf-8')
         )
 
-        return hash.hexdigest()
-    
+        return h.hexdigest()
+
+    def __str__(self):
+        return (
+            "Index: " + str(self.index) +
+            "\nTimestamp: " + str(self.timestamp) +
+            "\nData: " + str(self.data) +
+            "\nPrevious Hash: " + str(self.previous_hash) +
+            "\nHash: " + str(self.hash()) +
+            "\n----------"
+        )
+
