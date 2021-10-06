d438aa957de088ba6cc68e0a8587be5710c82abc
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-06 15:43:17 +0200

    Created block class

---
+++ main.py
@@ -0,0 +1,22 @@
+from hashlib import sha256
+from time import time
+
+class Block:
+    def __init__(self, index, data, previous_hash):
+        self.index = index
+        self.data = data
+        self.timestamp = time()
+        self.previous_hash = previous_hash
+        self.nonce = 0
+    
+    def compute_hash(self):
+        hash = sha256()
+        hash.update(
+            str(self.data).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') +
+            str(self.previous_hash).encode('utf-8') +
+            str(self.nonce).encode('utf-8')
+        )
+
+        return hash.hexdigest()
+    
