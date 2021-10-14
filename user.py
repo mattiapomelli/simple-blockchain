38c9d86002bc2de07aa0c5639951d9d09e1faf80
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-14 12:36:00 +0200

    adding users to file db

--- user.py
+++ user.py
@@ -1,14 +1,25 @@
 import uuid
+import json
 from transaction import Transaction
+
+def add_user(user):
+    f = open('db/users.json')
+    data = json.load(f)
+
+    data.append(user)
+
+    f = open('db/users.json', 'w')
+    json.dump(data, f)
 
 class User:
     def __init__(self, username, password):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.username = username
-        self.password = password # TODO: encrypt this password
+        self.password = password # TODO: encrypt this password with AES
+        add_user(self.__dict__)
 
     def perform_transaction(self, receiver_username, amount):
         return Transaction(self.username, receiver_username, amount)
 
     def __str__(self):
-        return "Id: " + str(self.id) + ", Username: " + self.username
+        return "Id: " + self.id + ", Username: " + self.username
