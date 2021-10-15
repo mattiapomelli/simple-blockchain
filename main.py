766dab2191ca46076ed4fd12644d33a806d079c9
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-15 17:50:16 +0200

    getting input to create users

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from blockchain import Blockchain
 from users_db import users_db
 
-users_db.reset()
+def main():
+    command = input('Select a command to execute: ')
 
-user1 = users_db.create_user('mattia', '12345678')
-user2 = users_db.create_user('julian', 'test_test')
-user3 = users_db.create_user('lukas', 'strong_password')
+    if command == 's':
+        username = input('Enter username: ')
+        password = input('Enter password: ')
+        user = users_db.create_user(username, password)
+    
+    else:
+        print('Invalid command')
 
-blockchain = Blockchain()
-
-t1 = user1.perform_transaction(user2.username, 30)
-t2 = user2.perform_transaction(user3.username, 20)
-t3 = user3.perform_transaction(user1.username, 45)
-
-blockchain.add_transaction(t1)
-blockchain.add_transaction(t2)
-blockchain.add_transaction(t3)
-blockchain.mine()
-
-print(blockchain)
+if __name__ == "__main__":
+    main()
