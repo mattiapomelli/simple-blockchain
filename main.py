9947a4d4dd83b1c461ee0eecb0338f6f99886f40
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-18 14:46:04 +0200

    performing transactions from cli

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from user_controller import user_controller 
+from user_controller import user_controller
+from users_db import users_db
+from transaction import Transaction
+from blockchain import Blockchain
 
 def main():
     current_user = None
+    blockchain = Blockchain()
 
     while True:
         # TODO: print list of available commands
@@ -19,16 +23,42 @@ def main():
             password = input('Enter password: ')
             user_controller.signin(username, password)
 
+        # check logged user
         elif command == 'u':
             if user_controller.current_user is None:
                 print("No user is logged in")
             else:
-                print("Logged user: " + user_controller.current_user.username)    
+                print("Logged user: " + user_controller.current_user.username)
 
+        # perform a new transaction
+        elif command == 't':
+            if user_controller.current_user is None:
+                print("You must be logged in to perform a transaction")
+                continue
+
+            receiver_username = input('Enter receiver username: ')
+            receiver = users_db.find_by_username(receiver_username)
+
+            if receiver is None:
+                print("No user with username " + receiver_username + " exists")
+                continue
+
+            amount = input('Enter amount: ')
+
+            transaction = Transaction(user_controller.current_user.username, receiver_username, amount)
+            blockchain.add_transaction(transaction)
+            print("Added transaction to pending transactions")
+        
+        elif command == 'pt':
+            print("Pending transactions:")
+            print(str([str(t) for t in blockchain.pending_transactions]))
+
+        # quit application
         elif command == 'q':
             print("Quitting application...")
             return
 
+        # invalid command
         else:
             print('Invalid command')
 
