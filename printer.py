447e92cdf56b03f1fe15a781c846a4841c168c40
Mattia Pomelli <mattiapomelli@gmail.com>
2021-10-23 15:52:37 +0200

    added end argument to printer methods

--- printer.py
+++ printer.py
@@ -4,16 +4,16 @@ class Printer:
         return "\033[38;2;{};{};{}m{}\033[38;2;255;255;255m".format(r, g, b, text)
 
     @staticmethod
-    def error(text):
+    def error(text, end = '\n'):
         colored_text = Printer.colored(247, 64, 54, text)
-        print(colored_text)
+        print(colored_text, end = end)
 
     @staticmethod
-    def success(text):
+    def success(text, end = '\n'):
         colored_text = Printer.colored(49, 235, 102, text)
-        print(colored_text)
+        print(colored_text, end = end)
 
     @staticmethod
-    def info(text):
+    def info(text, end = '\n'):
         colored_text = Printer.colored(252, 250, 174, text)
-        print(colored_text)
+        print(colored_text, end = end)
