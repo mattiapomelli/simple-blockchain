057474ee5ba7a908790715d15bfbd0d543c0f948
Mattia Pomelli <mattiapomelli@gmail.com>
2021-11-25 16:57:52 +0100

    storing hash of users db to check integrity

--- users_db.py
+++ users_db.py
@@ -1,5 +1,7 @@
 import json
 from user import User
+from Crypto.Hash import SHA256
+from printer import Printer
 
 class UserDB:
     """
@@ -8,10 +10,12 @@ class UserDB:
 
     db: list of all the users of the system
     db_path: path to the file storing user information
+    db_hash_path: path to the file storing the hash of the db
     """
 
     db = None
     db_path = 'db/users.json'
+    db_hash_path = 'db/users-hash.txt'
 
     def __init__(self):
         """
@@ -22,8 +26,20 @@ class UserDB:
         
         try:
             data = json.load(file)
+            db_hash = SHA256.new(str(data).encode()).hexdigest()
+            
+            hash_file = open(self.db_hash_path, 'r')
+            stored_hash = hash_file.read()
+
+            if (db_hash != stored_hash):
+                Printer.error("User database has has been corrupted: hashes don't match")
+                raise SystemExit
+
             # convert the users from json to User objects
             self.db = [User(u['username'], u['password'], u['email'], u['phone_nr'], u['address']) for u in data]
+            self.save_db_hash()
+        except SystemExit:
+            exit()
         except:
             self.reset()
 
@@ -39,8 +55,10 @@ class UserDB:
         # TODO: omptimization: don't rewrite the whole file, but just apped the new user
         # convert the users from User objects to json serializable dictionaries
         file = open(self.db_path, 'w')
-        serialized_db = [u.__dict__ for u in self.db]
-        json.dump(serialized_db, file, indent=4)
+        # serialized_db = [u.__dict__ for u in self.db]
+        json.dump(self.serialized_db, file, indent=4)
+
+        self.save_db_hash()
 
         return new_user
 
@@ -57,6 +75,20 @@ class UserDB:
         file = open(self.db_path, 'w')
         file.write(str([]))
         self.db = []
+        self.save_db_hash()
+
+    def save_db_hash(self):
+        file = open(self.db_hash_path, 'w')
+        db_hash = SHA256.new(str(self.serialized_db).encode()).hexdigest()
+        
+        file.write(db_hash)
+
+    @property
+    def serialized_db(self):
+        """
+        Returns the db content in a format writable to a file
+        """
+        return [u.__dict__ for u in self.db]
 
     def __str__(self):
         res = ""
